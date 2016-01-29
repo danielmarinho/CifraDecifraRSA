17b6ef9b86bd5feb5546088dbe2f40e585d6bad2
daniel_marinho <daniel_marinho@id.uff.br>
2016-01-29 17:23:47 -0200

    -Projeto de criptografia RSA criado

---
+++ cifra_decifra_rsa.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned short binExp(unsigned short b, int e, unsigned short n) {
+
+    unsigned int res = b;
+    unsigned int y = 1;
+
+    /* Caso base. */
+    if (e == 0) return (1);
+
+    while (e > 1) {
+
+        if (e & 1) {
+
+            /*
+             * Caso especial: expoente é ímpar.
+             * Acumular uma potência de ’res’ em ’y’.
+             */
+            y = (y * res) % (unsigned int) n;
+            e = e - 1;
+        }
+
+        /*
+         * Elevamos ’res’ ao quadrado, dividimos expoente por 2.
+         */
+        res = (res * res) % (unsigned int) n;
+        e = e / 2;
+    }
+
+    return ((unsigned short) ((res * y) % n));
+}
+
+void cifra(){
+    
+}
+
+void decifra(){
+    
+}
+
+int main(int argc, char** argv) {
+    printf("Ola Mundo");
+    return (EXIT_SUCCESS);
+}
+
